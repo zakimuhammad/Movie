262b7e3dcb7a3f9a13414cabc9f76f4a3f2eb559
zaki <zaki@twiscoder.com>
2022-09-30 09:06:54 +0700

    remove unused lib

--- app/src/main/java/com/zaki/movieapp/di/NetworkModule.kt
+++ app/src/main/java/com/zaki/movieapp/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.zaki.movieapp.di
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.zaki.movieapp.data.remote.api.MovieApiService
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -10,11 +11,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
     @Provides
+    @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         val interceptor = HttpLoggingInterceptor()
         return OkHttpClient.Builder()
@@ -23,6 +26,7 @@ class NetworkModule {
     }
 
     @Provides
+    @Singleton
     fun provideGson(): Gson {
         return GsonBuilder()
             .setLenient()
@@ -30,6 +34,7 @@ class NetworkModule {
     }
 
     @Provides
+    @Singleton
     fun provideMovieApiService(
         gson: Gson,
         okHttpClient: OkHttpClient
